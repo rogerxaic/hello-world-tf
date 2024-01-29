159e38b368962462008b25d51220ab6eb3384094
Roger Miret <rogerxaic@users.noreply.github.com>
2024-01-29 16:28:52 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+variable "ibmcloud_api_key" {
+  default = null
+}
+
+provider "ibm" {
+  region              = "eu-de"
+  visibility          = "public"
+  ibmcloud_api_key    = var.ibmcloud_api_key
+}
+
+resource "ibm_is_vpc" "example" {
+  name = "example-vpc"
+}
+
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    # Use "greater than or equal to" range in modules
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.59.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+  }
+}
